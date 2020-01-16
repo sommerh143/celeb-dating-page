1e63baf6da23e2752897d2316612b8de0e716b38
Student <example@example.com>
2020-01-16 14:15:44 -0800

    wokring button changing celeb when option change

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,20 @@ $(document).ready(function () {
   
   $("#result-button").click(function () {
     var food = $("input:radio[name=food]:checked").val();
-      if (food === "Mexican") {
+    var music = $("input:radio[name=music]:checked").val();
+      if (food === "Mexican" && music === "Rock" || music === "EDM") {
       $("#santapic").show();
-    } else {
+      $("#buddypic").hide();
+      $("#rudolphpic").hide();
+     
+    } else if (food ==="Vegetarian" && music == "Rap") {
+      $("#buddypic").show();
       $("#santapic").hide();
+      $("#rudolphpic").hide();
+    } else {
+      $("#rudolphpic").show();
+      $("#santapic").hide();
+      $("#buddypic").hide();
     }
     event.preventDefault();
   });
