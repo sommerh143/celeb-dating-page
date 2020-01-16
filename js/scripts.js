2bcb0798f79a13d9001ded16b619b1ae71a2b498
Student <example@example.com>
2020-01-16 11:52:03 -0800

    creat celeb dating page, ask questions with submit button

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+    var food = $("input:radio[name=food]:checked").val();
+      $("#result-button").click(function(){
+        $(document).ready(function(){
+        if (food = "Mexican") {
+           $("#santapic").show();
+        } 
+       
+  event.preventDefault();
+  });   
+ });
+  
+ 
